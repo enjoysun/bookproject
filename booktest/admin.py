fe9d51bb3ee926d29da1af8b4b4d39587e208a1e
you <1570819807@qq.com>
2017-10-31 23:48:27 +0800

    了解嵌入自定义admin模块

--- booktest/admin.py
+++ booktest/admin.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 #注：引用当前项目下模块，2.7.x版本直接from模块名即可，而3.x需要.模块名
 from models import *
 #自定义admin模块显示：1.创建自定义模块类(继承自admin.ModelAdmin) 2.注册
+
+#嵌入类(继承自admin.StackedInline即为列表展示效果,继承自admin.TabularInline为表格显示效果)
+class HeroInfoInline(admin.TabularInline):
+    model = Hero#注册嵌入模型类
+    extra = 3#注册展示几个
+
 class Bookinfoadmin(admin.ModelAdmin):
     #1.列表显示页面自定义：
 
@@ -21,5 +27,7 @@ class Bookinfoadmin(admin.ModelAdmin):
         ('基础',{'fields':['bname']}),
         ('时间',{'fields':['bpub_date']})
     ]
+    #inlines注册嵌入类
+    inlines = [HeroInfoInline]
 admin.site.register(Book,Bookinfoadmin)
 admin.site.register(Hero)
